3369bdb550998716d7486e992ff95c243a71d619
phatbt <btphat.dev@gmail.com>
2023-05-04 15:35:28 +0700

    first commit

---
+++ src/main/java/com/btphat/didongvietclone/DidongvietCloneApplication.java
@@ -0,0 +1,13 @@
+package com.btphat.didongvietclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidongvietCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidongvietCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btphat/didongvietclone/DidongvietCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btphat.didongvietclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidongvietCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
